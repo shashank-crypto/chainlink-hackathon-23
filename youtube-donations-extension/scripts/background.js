a0d8a3e6cf135fcb89ba592a5297339a21f88ec7
Matt Sampson <masampso@gmail.com>
2023-06-04 11:43:32 -0700

    removed exposed api endpoint

--- youtube-donations-extension/scripts/background.js
+++ youtube-donations-extension/scripts/background.js
@@ -10,9 +10,8 @@ chrome.runtime.onConnect.addListener((port) => {
             try {
                 const youtubeName = request.youtubeName;
                 console.log(youtubeName);
-                const response = await fetch(
-                    `https://e3qo0ohe2c.execute-api.us-west-1.amazonaws.com/Prod/youtube-donations/${youtubeName}`
-                );
+                const response = await fetch();
+                /*"endpoint here"*/
                 const data = await response.json();
                 console.log(data);
                 const parsedBody = JSON.parse(data.body);
